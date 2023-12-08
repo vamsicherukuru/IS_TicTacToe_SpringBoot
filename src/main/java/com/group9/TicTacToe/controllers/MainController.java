7b771a28d76c052b28b65254076617e8ee08d4c3
cvams <cvamsifeb21@gmail.com>
2023-12-08 02:07:30 -0500

    Console Printable

--- src/main/java/com/group9/TicTacToe/controllers/MainController.java
+++ src/main/java/com/group9/TicTacToe/controllers/MainController.java
@@ -2,7 +2,9 @@ package com.group9.TicTacToe.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -15,8 +17,11 @@ public class MainController {
         return "home";
     }
     @GetMapping("/gameplay")
-    public String gameplayPage(){
-
+    public String gameplayPage(@RequestParam("difficultyLevel") String difficultyLevel,
+                               @RequestParam("playerName") String playerName,
+                               Model model){
+        model.addAttribute("difficultyLevel", difficultyLevel);
+        model.addAttribute("playerName", playerName);
 
         return "game";
     }
